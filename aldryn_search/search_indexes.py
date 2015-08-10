d01ce0aeb34f2d0b4b042e5f12be9f85ae9fc6bb
José L. Patiño <jose.lpa@gmail.com>
2015-08-10 14:37:49 +0100

    Filtering pages title results per site.

--- aldryn_search/search_indexes.py
+++ aldryn_search/search_indexes.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
 from django.db.models import Q
 from django.utils import timezone
 
@@ -10,6 +12,8 @@ from .utils import clean_join, get_index_base, strip_tags
 
 # Backwards compatibility
 _strip_tags = strip_tags
+
+site_pk = Site.objects.get_current().pk
 
 
 class TitleIndex(get_index_base()):
@@ -137,7 +141,7 @@ class TitleIndex(get_index_base()):
         queryset = Title.objects.public().filter(
             Q(page__publication_date__lt=timezone.now()) | Q(page__publication_date__isnull=True),
             Q(page__publication_end_date__gte=timezone.now()) | Q(page__publication_end_date__isnull=True),
-            Q(redirect__exact='') | Q(redirect__isnull=True),
+            Q(redirect__exact='') | Q(redirect__isnull=True), page__site_id=site_pk,
             language=language
         ).select_related('page').distinct()
         return queryset
